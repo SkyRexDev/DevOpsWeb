a2a7347466326419665bc04b64ec2cf0a52a2aaf
SkyRexDev <skyrexhd@gmail.com>
2022-04-23 19:56:50 +0100

    Initial commit.

---
+++ src/main/java/com/wookipedia/WookipediaApplication.java
@@ -0,0 +1,13 @@
+package com.wookipedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WookipediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WookipediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wookipedia/WookipediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wookipedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WookipediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
